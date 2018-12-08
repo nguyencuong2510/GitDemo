1fa7ddf8d14bf742a676c6df934142975af0ec42
admin <cuong251095@gmail.com>
2018-12-08 23:22:20 +0700

    commit is master

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("ngueyn")
+        print("commit is master")
     }
 
     
